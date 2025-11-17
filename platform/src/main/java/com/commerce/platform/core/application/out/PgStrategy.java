56537cb16ada47d6e2749c2cdfc89a7250ac1be3
SinJeongEun <lisasje25@gmail.com>
2025-11-17 21:51:21 +0900

    feature: PgProvider 에서 pg사별 지원 결제유형, 지원사 관리한다.

--- platform/src/main/java/com/commerce/platform/core/application/out/PgStrategy.java
+++ platform/src/main/java/com/commerce/platform/core/application/out/PgStrategy.java
@@ -1,59 +1,16 @@
 package com.commerce.platform.core.application.out;
 
-import com.commerce.platform.core.application.in.dto.PayCancelCommand;
-import com.commerce.platform.core.application.in.dto.PayOrderCommand;
-import com.commerce.platform.core.application.out.dto.PgPayResponse;
 import com.commerce.platform.core.domain.enums.PgProvider;
 
 public abstract class PgStrategy {
 
+    /**
+     * pg사별 요청에 따라 [Card | Easy | Phone]PayService 구현체 실행한다.
+     * @param request todo dto
+     * @return todo 결재응답dto
+     */
+    public abstract String process(String request);
+
     public abstract PgProvider getPgProvider();
-
-    /**
-     * 라우팅된 pg사에서 해당 결제유형으로 승인 요청
-     * @param command
-     * @return pg 응답
-     */
-    public final PgPayResponse processApproval(PayOrderCommand command) {
-        return switch (command.getPayMethod()) {
-            case CARD -> getCardPay().approveCard(command);
-            case EASY_PAY -> getEasyPay().approveEasyPay(command);
-            case PHONE -> getPhonePay().approvePhone(command);
-        };
-    }
-
-    /**
-     * 라우팅된 pg사에서 해당 결제유형으로 취소 요청
-     * @param command
-     * @return pg 응답
-     */
-    public final PgPayResponse processCancel(PayCancelCommand command) {
-        return switch (command.getPayMethod()) {
-            case CARD -> getCardPay().cancelCard(command);
-            case EASY_PAY -> getEasyPay().cancelEasyPay(command);
-            case PHONE -> getPhonePay().cancelPhone(command);
-        };
-    }
-
-    private CardPay getCardPay() {
-        if (this instanceof CardPay cardPay) {
-            return cardPay;
-        }
-        throw new UnsupportedOperationException("카드결제 미지원 pg사");
-    }
-
-    private PhonePay getPhonePay() {
-        if (this instanceof PhonePay phonePay) {
-            return phonePay;
-        }
-        throw new UnsupportedOperationException("휴대폰결제 미지원 pg사");
-    }
-
-    private EasyPay getEasyPay() {
-        if (this instanceof EasyPay easyPay) {
-            return easyPay;
-        }
-        throw new UnsupportedOperationException("간편결제 미지원 pg사");
-    }
 
 }
--- platform/src/main/java/com/commerce/platform/infrastructure/pg/TossStrategy.java
+++ platform/src/main/java/com/commerce/platform/infrastructure/pg/TossStrategy.java
@@ -1,41 +1,19 @@
 package com.commerce.platform.infrastructure.pg;
 
-import com.commerce.platform.core.application.in.dto.PayCancelCommand;
-import com.commerce.platform.core.application.in.dto.PayOrderCommand;
-import com.commerce.platform.core.application.out.CardPay;
-import com.commerce.platform.core.application.out.EasyPay;
 import com.commerce.platform.core.application.out.PgStrategy;
-import com.commerce.platform.core.application.out.dto.PgPayResponse;
 import com.commerce.platform.core.domain.enums.PgProvider;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TossStrategy extends PgStrategy
-        implements CardPay, EasyPay {
+public class TossStrategy extends PgStrategy {
+
+    @Override
+    public String process(String request) {
+        return "";
+    }
 
     @Override
     public PgProvider getPgProvider() {
         return PgProvider.TOSS;
-    }
-
-
-    @Override
-    public PgPayResponse approveCard(PayOrderCommand command) {
-        return null;
-    }
-
-    @Override
-    public PgPayResponse cancelCard(PayCancelCommand command) {
-        return null;
-    }
-
-    @Override
-    public PgPayResponse approveEasyPay(PayOrderCommand command) {
-        return null;
-    }
-
-    @Override
-    public PgPayResponse cancelEasyPay(PayCancelCommand command) {
-        return null;
     }
 }
